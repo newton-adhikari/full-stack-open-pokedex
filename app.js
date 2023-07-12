da35243b451fbfbebda1ff3dbf7053ddbfce8b37
Newton Adhikari <neadh072@kathmandu>
2023-07-12 14:32:46 +0545

    changed the route to get /

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ const PORT = typeof process !== 'undefined' ? process.env.PORT || 5000 : 5000;
 
 app.use(express.static('dist'))
 
+app.get('/', () => {
+  console.log('hello from express')
+})
+
 app.listen(PORT, () => {
   console.log('server started on port 5000')
 })
